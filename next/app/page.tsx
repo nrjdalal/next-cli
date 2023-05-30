b97e54a085f6e634acacd3e13ecd7ff37d8d0884
Neeraj Dalal <admin@nrjdalal.com>
2023-05-30 12:32:25 +0530

    added login button

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -7,7 +7,7 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        <h1 className="text-3xl font-extrabold leading-tight md:text-4xl">
           Beautifully designed components <br className="hidden sm:inline" />
           built with Radix UI and Tailwind CSS.
         </h1>
@@ -18,12 +18,12 @@ export default function IndexPage() {
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href={'/access'}
           target="_blank"
           rel="noreferrer"
           className={buttonVariants()}
         >
-          Documentation
+          Login / Register
         </Link>
         <Link
           target="_blank"
