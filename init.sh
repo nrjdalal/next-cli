720a0ce147ef1396178dd7fe53dd84a29c0a7139
Neeraj Dalal <admin@nrjdalal.com>
2023-05-30 11:49:36 +0530

    added fonts

--- init.sh
+++ init.sh
@@ -10,15 +10,12 @@ pnpm create next-app --ts --tailwind --eslint --app --import-alias "@/*" --use-p
 
 AUTORESPONSE
 
-# moving devDependencies from dependencies to devDependencies
 pnpm add -D $(npx depcheck --oneline | awk 'NR==2')
 pnpm add -D $(cat package.json | grep 'eslint' | awk -F\" '{print $2}' | xargs)
 pnpm add -D $(cat package.json | grep 'typescript' | awk -F\" '{print $2}' | xargs)
 
-# adding useful dependencies
 pnpm add sharp
 
-# updating .gitignore
 cat >>.gitignore <<EOF
 
 # files
@@ -29,24 +26,21 @@ yarn.lock
 /.vscode
 EOF
 
-# adding prettier and plugins
 pnpm add -D @ianvs/prettier-plugin-sort-imports prettier prettier-plugin-tailwindcss
 curl -s "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/.prettier.config.js?v=$RANDOM" | cat >.prettier.config.js
 
-# making repository compatible with shadcn/ui
 pnpx shadcn-ui init <<AUTORESPONSE
 yes
 AUTORESPONSE
 
-# configuring .eslintrc.json
-curl -s "https://raw.githubusercontent.com/shadcn/next-template/main/.eslintrc.json?v=$RANDOM" | cat >.eslintrc.json
+curl -s "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/.eslintrc.json?v=$RANDOM" | cat >.eslintrc.json
 
-# configuring auth via next-auth and prisma
 pnpm add @next-auth/prisma-adapter @prisma/client next-auth && pnpm add -D prisma
 
 mkdir -p lib
-curl "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/lib/db.ts?v=$RANDOM" | cat >lib/db.ts
 curl "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/lib/auth.ts?v=$RANDOM" | cat >lib/auth.ts
+curl "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/lib/db.ts?v=$RANDOM" | cat >lib/db.ts
+curl "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/lib/fonts.ts?v=$RANDOM" | cat >lib/fonts.ts
 curl "https://raw.githubusercontent.com/nrjdalal/next-cli/main/next/lib/session.ts?v=$RANDOM" | cat >lib/session.ts
 
 mkdir -p types
